a571acc6fbdd1869e678cb7d0ce3bb2b100dd6f4
david.ongenaert <bion@disroot.org>
2023-05-30 00:42:28 +0200

    v4.7

---
+++ dev-resources/package.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ..
+
+rm -f firefox.zip
+mkdir -p temp
+
+cp popup.html temp
+cp manifest-firefox.json temp/manifest.json
+cp LICENSE temp
+cp -r scripts temp
+cp -r ext-resources temp
+
+cd temp
+zip -r ../firefox.zip *
+cd ..
+rm -r temp
+
+
+
+rm -f chrome.zip
+mkdir -p temp
+
+cp popup.html temp
+cp manifest-chrome.json temp/manifest.json
+cp LICENSE temp
+cp -r scripts temp
+cp -r ext-resources temp
+
+cd temp
+zip -r ../chrome.zip *
+cd ..
+rm -r temp
